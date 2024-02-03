747caff5bf713b2378a7a2f381a677c439da0437
mharb98 <marwanharb65@outlook.com>
2024-02-03 14:05:21 +0200

    removed unwanted entities

--- src/data-access/entities/dido.entity.ts
+++
@@ -1,5 +0,0 @@
-import { Entity } from 'typeorm';
-import { BaseEntity } from './base.entity';
-
-@Entity({ name: 'didos' })
-export class DidoEntity extends BaseEntity {}
--- src/data-access/entities/yasseen.entity.ts
+++
@@ -1,5 +0,0 @@
-import { Entity } from 'typeorm';
-import { BaseEntity } from './base.entity';
-
-@Entity({ name: 'yasseens' })
-export class YasseenEntity extends BaseEntity {}
